0fa1408827f96dc680d606ea9404c66d76e9993e
Dmitry Morozov <divelineq@gmail.com>
2025-05-04 22:11:20 +0700

    chore: add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     tailwindcss(),
     react()
   ],
