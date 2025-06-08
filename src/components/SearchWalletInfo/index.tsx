aa2a71f2bf81fb340d5051b5da5e366e2a87db99
Dmitry Morozov <divelineq@gmail.com>
2025-06-08 13:52:38 +0700

    feat: отображение ошибки или валидного значения адреса

--- src/components/SearchWalletInfo/index.tsx
+++ src/components/SearchWalletInfo/index.tsx
@@ -1,24 +1,12 @@
-import { useForm } from "@tanstack/react-form";
-import type { z } from "zod/v4";
-import type { ADRESS_VALIDATION_SCHEMA } from "./ValidationSchema";
 import { detectCryptoAddressNetwork } from "./detectCryptoAddressNetwork";
-
-type Adress = {
-	adress: z.infer<typeof ADRESS_VALIDATION_SCHEMA>;
-};
-
-const defaultAdress: Adress = { adress: "" };
+import { useWalletForm } from "./useWalletForm";
 
 function SearchWalletInfo() {
-	const form = useForm({
-		defaultValues: defaultAdress,
-		onSubmit: (values) => {
-			console.log(values);
-		},
-	});
+	const form = useWalletForm();
 
 	return (
 		<form
+			className="flex flex-col items-center"
 			onSubmit={(e) => {
 				e.stopPropagation();
 				e.preventDefault();
@@ -37,30 +25,36 @@ function SearchWalletInfo() {
 					const detection = detectCryptoAddressNetwork(inputValue);
 					const showNetwork = detection.isValid && detection.network;
 					return (
-						<div>
-							<div>
-								<input
-									placeholder="Enter wallet address"
-									className="m-4 border-2 border-gray-500 rounded-sm"
-									value={field.state.value}
-									onBlur={field.handleBlur}
-									onChange={(e) => field.handleChange(e.target.value)}
-								/>
+						<div className="flex flex-col items-center w-[420px]">
+							<label
+								htmlFor="wallet-adress"
+								className="text-sm mt-6 flex gap-1 w-full justify-between"
+							>
+								<p>Wallet address</p>
 								{field.state.meta.errors && (
-									<div className="text-red-500 px-2 text-md">
+									<div className="text-red-500 text-md">
 										{field.state.meta.errors}
 									</div>
 								)}
 								{showNetwork && (
-									<div className="text-green-500 px-2 text-md">
+									<div className="text-green-500 text-md">
 										{detection.network}
 									</div>
 								)}
-							</div>
+							</label>
+							<input
+								id="wallet-adress"
+								placeholder="Enter wallet address"
+								className="p-2 w-full  border-2 border-gray-500 rounded-sm"
+								value={field.state.value}
+								onBlur={field.handleBlur}
+								onChange={(e) => field.handleChange(e.target.value)}
+							/>
 							<button
+								disabled={!detection.isValid}
 								type="submit"
 								onClick={form.handleSubmit}
-								className="border-2 border-gray-500 rounded-sm p-1 m-2"
+								className="bg-blue-600 text-white py-1 rounded-sm w-full my-2 cursor-pointer hover:bg-blue-800 transition-colors disabled:bg-gray-600 disabled:cursor-not-allowed"
 							>
 								Search
 							</button>
