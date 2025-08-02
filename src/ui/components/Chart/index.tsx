011ea0ec6c951f3b94b796a13c1f2d446a331198
Dmitry Morozov <divelineq@gmail.com>
2025-08-02 20:27:33 +0700

    feat: данные подставляются правильно с вебсокета для графика

--- src/ui/components/Chart/index.tsx
+++ src/ui/components/Chart/index.tsx
@@ -5,6 +5,9 @@ import {
 	ColorType,
 	CrosshairMode,
 	type DeepPartial,
+	type IChartApi,
+	type ISeriesApi,
+	LineSeries,
 	type OhlcData,
 	type Time,
 	createChart,
@@ -19,55 +22,78 @@ const CANDLESTICK_COLORS: Partial<CandlestickStyleOptions> = {
 	wickDownColor: "#ef5350",
 };
 
+const CHART_OPTIONS = {
+	autoScale: true,
+	layout: {
+		background: { type: ColorType.Solid, color: "#101014" },
+		textColor: "white",
+	},
+	autoSize: true,
+	grid: {
+		vertLines: {
+			color: "#212121",
+		},
+		horzLines: {
+			color: "#212121",
+		},
+	},
+	crosshair: {
+		mode: CrosshairMode.Normal,
+		horzLine: {
+			visible: true,
+			color: "white",
+			labelVisible: true,
+		},
+		vertLine: {
+			visible: true,
+			color: "white",
+			labelVisible: true,
+		},
+	},
+} as DeepPartial<ChartOptions>;
+
 type Props = {
 	data: OhlcData[];
+	newData: OhlcData;
 	className?: string;
 	chartStyle?: { width: number; height: number };
 };
 
-function Chart({ data }: Props) {
-	const candlestickSeries = useRef<HTMLDivElement>(null);
+function Chart({ data, newData }: Props) {
+	const chartRef = useRef<IChartApi | null>(null);
+	const candlestickSeriesRef = useRef<ISeriesApi<"Candlestick"> | null>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const newDataRef = useRef<OhlcData | null>(null);
 
 	useEffect(() => {
-		const chart = createChart(candlestickSeries.current!, {
-			layout: {
-				background: { type: ColorType.Solid, color: "#101014" },
-				textColor: "white",
-			},
-			autoSize: true,
-			timeScale: {
-				rightOffset: 10,
-				rightBarStaysOnScroll: true,
-			},
-			grid: {
-				vertLines: {
-					color: "#212121",
-				},
-				horzLines: {
-					color: "#212121",
-				},
-			},
-			crosshair: {
-				mode: CrosshairMode.Normal,
-				horzLine: {
-					visible: true,
-					color: "white",
-					labelVisible: true,
-				},
-				vertLine: {
-					visible: true,
-					color: "white",
-					labelVisible: true,
-				},
-			},
-		} as DeepPartial<ChartOptions>);
-		chart.timeScale().fitContent();
+		if (!containerRef.current) return;
 
-		const newSeries = chart.addSeries(CandlestickSeries, CANDLESTICK_COLORS);
-		newSeries.setData(data);
+		const lineData = data.map((item) => ({
+			time: item.time,
+			value: (item.open + item.close) / 2,
+		}));
+
+		const chart = createChart(containerRef.current, CHART_OPTIONS);
+		chart.timeScale().fitContent();
+		chartRef.current = chart;
+
+		const lineSeries = chart.addSeries(LineSeries, {
+			lineWidth: 1,
+			color: "#fff",
+			lastValueVisible: false,
+			crosshairMarkerVisible: false,
+		});
+		lineSeries.setData(lineData);
+
+		const candlestickSeries = chart.addSeries(
+			CandlestickSeries,
+			CANDLESTICK_COLORS,
+		);
+		candlestickSeries.setData(data);
+		candlestickSeriesRef.current = candlestickSeries;
 
 		const handleResize = () => {
-			chart.applyOptions({ width: candlestickSeries.current?.clientWidth });
+			chart.applyOptions({ width: containerRef.current?.clientWidth });
 		};
 
 		const from = data.length - 100;
@@ -85,11 +111,16 @@ function Chart({ data }: Props) {
 
 			chart.remove();
 		};
-	}, [data]);
+	}, []);
 
-	return (
-		<div ref={candlestickSeries} style={{ width: "100%", height: "100%" }} />
-	);
+	useEffect(() => {
+		if (newDataRef.current !== newData) {
+			candlestickSeriesRef.current?.update(newData);
+			newDataRef.current = newData;
+		}
+	}, [newData, candlestickSeriesRef.current]);
+
+	return <div ref={containerRef} style={{ width: "100%", height: "100%" }} />;
 }
 
 export { Chart };
