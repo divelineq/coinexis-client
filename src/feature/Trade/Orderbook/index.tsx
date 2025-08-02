3fb284c3e2b69b32ddab0f0d27ad14cb773f6af9
Dmitry Morozov <divelineq@gmail.com>
2025-08-03 01:44:27 +0700

    refactor: orderbook div

--- src/feature/Trade/Orderbook/index.tsx
+++ src/feature/Trade/Orderbook/index.tsx
@@ -10,12 +10,14 @@ function Orderbook({ symbol, depth }: Props) {
 	return (
 		<div className="w-full bg-[#101014] p-2 flex flex-col">
 			<div className="text-sm font-semibold mb-2">Order Book</div>
-			<div className="w-full flex flex-col justify-between text-[14px] gap-2">
-				<div>
-					<OrderbookList data={orderbook.bids} color="var(--sell-color)" />
-				</div>
-				<div>
-					<OrderbookList data={orderbook.asks} color="var(--buy-color)" />
+			<div className="flex flex-col gap-2 h-full">
+				<div className="w-full flex flex-col justify-between text-[14px] gap-2">
+					<div>
+						<OrderbookList data={orderbook.bids} color="var(--sell-color)" />
+					</div>
+					<div>
+						<OrderbookList data={orderbook.asks} color="var(--buy-color)" />
+					</div>
 				</div>
 				<div className="mt-2 text-[14px]">
 					<PercentBar bids={orderbook.bids} asks={orderbook.asks} />
